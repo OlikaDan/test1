76d9da2a1da9696abc75393e90ae8a15ad042f07
OlikaDan <oliya.daniel@gmail.com>
2021-03-25 20:10:42 +0300

    Первая выгрузка на Github

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Командная работа на Github");
+
+        //System.out.println("Сохранить изменения в основной ветке///");
+    }
+}
